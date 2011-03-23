016d123f9f41a21d2916e6cb2425a5ba4d0f8dc0
Whit Morriss <whit@surveymonkey.com>
2011-03-23 13:27:24 -0700

    Finished test coverage and middlewware

--- husherror/middleware.py
+++ husherror/middleware.py
@@ -1,13 +1,16 @@
-from collections import defaultdict
 from husherror.limiter import Limiter
-from itertools import count
+import traceback
+import sys
+from webob import Response
 from webob import dec
+import json
 import logging
+
 
 logger = logging.getLogger(__name__)
 
 
-class HushMiddleWare(object):
+class Hush(object):
     """
     A middleware for working with weberror to prevent flooding in
     certain mission critical situations.
@@ -15,27 +18,99 @@ class HushMiddleWare(object):
 
     limiter_class = Limiter
     
-    def __init__(self, app, period, threshold, noraise_response, exc_whitelist=set(), exc_blacklist=set()):
+    def __init__(self, app, period=1, threshold=3, release=10, noraise=None, use_py_hash=False, exc_whitelist=set(), exc_blacklist=set(), store=None):
+        """
+        
+        """
         self.app = app
         self.whitelist = exc_whitelist
         self.blacklist = exc_blacklist
-        self.noraise_response
-        self.limiter = self.limiter_class(threshold, period)
-        #self._mem_hit_count = defaultdict(count)
+        if store is None:
+            self.limiter = self.limiter_class(period, threshold, release)
+        else:
+            self.limiter = self.limiter_class(period, threshold, release, store=store)
+        self.noraise = noraise
+        self.use_py_hash = use_py_hash
+        if self.noraise is None:
+            self.noraise = self.default_noraise
 
-    def hash_exc(self):
-        pass
+    def default_noraise(self, request, (et, ev, etb)):
+        body = json.dumps(dict(error="%s" %ev.message))
+        return Response(body=body, status=400, headers={'Content-Type': 'application/json'})
 
-    @dec.wsgify
-    def __call__(self, request):
+    def noraise_response(self, request, exc):
+        """
+        Calls whatever function has been set to generate a response
+        for the captured exceptions.
+        """
+        return self.noraise(request, exc)
+
+    def hash_exception(self, request, (etype, eval, etb)):
+        """
+        Creates a semi unique key for an exception based on the the
+        following 5 characteristics of an exception::
+
+        From where the exc occurred:
+        
+        * name of function 
+        * truncated filename for module 
+        * index of byte string of the code object
+
+        Also:
+        * Path info for original
+        * name of exception type
+
+        The resulting tuple is serialized. Depending on whether the
+        middleware object is configured to use the python hash builtin
+        or not, the serialize version is hashed. Resulting key is
+        returned.
+        """
+        block_name = etb.tb_frame.f_code.co_name
+        filename = "/".join((etb.tb_frame.f_code.co_filename.split('/')[-2:]))
+        lineno = etb.tb_lineno
+        last_instr = etb.tb_lasti
+        identifier = (request.path_info, etype.__name__, filename, block_name, "%s.%s" %(lineno, last_instr))
+        serialized = ":".join(identifier)
+        if self.use_py_hash is True:
+            return hash(serialized)
+        return serialized
+
+    def gated_response(self, request, (et, ev, etb)):
+        """
+        Checks limiter to determine whether an exception should be
+        squelched or re-raised.
+        """
+        key = self.hash_exception(request, (et, ev, etb))
+        gate = self.limiter.gate(key)
+        if gate is True:
+            return self.noraise_response(request, (et, ev, etb))
+        raise ev
+
+    def handle_request(self, request):
+        """
+        Attempt to return a valid response from the wrapped
+        application. If it fails, proceed to check exception for
+        inclusion in whitelisted or blacklisted exceptions, then pass
+        to `gated_response`.
+
+        Takes a request, returns a response.
+        """
         try:
             return request.get_response(self.app)
         except Exception as e:
             if type(e) in self.whitelist:
                 raise e
+            exc_info = sys.exc_info()
             if type(e) in self.blacklist:
-                return 
-#
+                return self.noraise_response(request, exc_info)
+            return self.gated_response(request, exc_info)
+
+    @dec.wsgify
+    def __call__(self, request):
+        """
+        Provide a wsgi for `handle_request`
+        """
+        return self.handle_request(request)
 
 
 
--- husherror/tests/test_middleware.py
+++ husherror/tests/test_middleware.py
@@ -1,10 +1,121 @@
+from husherror import store
+from mock import Mock
+from nose.tools import raises
+from webob import Response
+from webob import dec
+from webtest import TestApp
+import json
+import sys
 import unittest
 
+
+boom = Exception("KABOOM")
+
+class TestException(Exception):
+    """
+    A test exception
+    """
 
 class TestMW(unittest.TestCase):
 
     def setUp(self):
+        from husherror import middleware as mw
+        self.request = Mock()
+        self.request.path_info = '/mypath'
+        self.request.get_response = Mock(side_effect=boom, name='response')
+        self.mod = mw
+
+    def make_one(self, app, **kw):
+        return self.mod.Hush(app, store=store.InMemoryStore(), **kw)
+
+    def test_init(self):
+        newhush = self.mod.Hush(Mock())
+        assert newhush.limiter.store
+
+    def test_hash_exc(self):
         pass
 
-    def test_middleware(self):
-        pass
+    @raises(Exception)
+    def test_basic_exeception_reraised(self):
+        app = self.make_one(Mock())
+        app.handle_request(self.request)
+    
+    def test_basic_exception_blacklist(self):
+        self.request.get_response = Mock(side_effect=TestException('KABOOM'), name='response')
+        app = self.make_one(Mock(), exc_blacklist=set((TestException,)))
+        res = app.handle_request(self.request)
+        assert res.headers['Content-Type'] == 'application/json'
+        assert json.loads(res.body)['error'] == 'KABOOM'
+
+    def test_squelch(self):
+        app = self.make_one(Mock())
+        self.request.get_response = Mock(side_effect=TestException('KABOOM'), name='response')
+        for i in xrange(app.limiter.threshold):
+            try:
+                app.handle_request(self.request)
+                assert False, "Exceptions not being bubbled"
+            except TestException:
+                pass
+
+        # now the threshold should be exceeded
+        res = app.handle_request(self.request)
+        assert res.headers['Content-Type'] == 'application/json'
+        assert json.loads(res.body)['error'] == 'KABOOM'
+
+    @raises(TestException)
+    def test_whitelist(self):
+        app = self.make_one(Mock(), exc_whitelist=set((TestException,)))
+        self.request.get_response = Mock(side_effect=TestException('KABOOM'), name='response')
+        for i in xrange(app.limiter.threshold):
+            try:
+                app.handle_request(self.request)
+                assert False, "Exception should always be raised if whitelisted"
+            except TestException:
+                pass
+
+        # now the threshold should be exceeded
+        # but the exception should still be raised
+        app.handle_request(self.request)
+
+    def test_hashing(self):
+        self.request.get_response = Mock(side_effect=TestException('KABOOM'), name='response')
+        app1 = self.make_one(Mock(), use_py_hash=True)
+        app2 = self.make_one(Mock(), use_py_hash=False)
+        try:
+            app1.handle_request(self.request)
+        except TestException:
+            exc = sys.exc_info()
+            key1 = app1.hash_exception(self.request, exc)
+            assert isinstance(key1, int)
+
+        try:
+            app2.handle_request(self.request)
+        except TestException:
+            exc = sys.exc_info()
+            key2 = app2.hash_exception(self.request, exc)
+            assert isinstance(key2, basestring), key2
+            assert key2.startswith('/mypath:TestException:tests/test_middleware.py:test_hashing:'), key2
+
+        try:
+            app2.handle_request(self.request)
+        except TestException:
+            exc = sys.exc_info()
+            key2 = app2.hash_exception(self.request, exc)
+            app2.use_py_hash = True
+            key1 = app2.hash_exception(self.request, exc)
+            assert hash(key2) == key1, "%s != %s" %(hash(key2), key1)
+
+
+def test_mw_wsgi_call():
+    """
+    Test that our webob dispatch works (to complete coverage)
+    """
+    from husherror import middleware as mw
+    app = TestApp(mw.Hush(dummy))
+    res = app.get('/')
+    assert res and res.body == 'Ok'
+
+
+@dec.wsgify
+def dummy(request):
+    return Response("Ok")
